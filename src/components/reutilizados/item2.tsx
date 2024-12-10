9835106646f41f5111fe9e78b7e9a4e409b12c3c
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-12-10 10:24:35 -0300

    pequenas alterações no item2

--- src/components/reutilizados/item2.tsx
+++ src/components/reutilizados/item2.tsx
@@ -9,22 +9,25 @@ export const Item2 = ({ data }: Props) => {
     return (
         <div className="grid grid-cols-4 gap-4 2xl:grid-cols-5">
             {data.map((item: MediaItem) => (
-                <div>
+                <div className="transition-all border-2 border-transparent hover:scale-110 hover:border-white hover:rounded-lg">
                     {
                         item.media_type === "movie" &&
                         <div>
-                            <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.original_title} />
-                            <div>
+                            <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.original_title} className="hover:rounded-t-lg transition-all"/>
+                            <div className="pl-2 pb-2">
                                 <h2 className="font-bold pt-2">{item.title}</h2>
+                                <p>Filme</p>
+                                <p>{funcaoData(item.release_date)}</p>
                             </div>
                         </div>
                     }
                     {
                         item.media_type === "tv" &&
                         <div>
-                            <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.original_name} />
-                            <div>
+                            <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.original_name} className="hover:rounded-t-lg transition-all"/>
+                            <div className="pl-2 pb-2">
                                 <h2 className="font-bold pt-2">{item.name}</h2>
+                                <p>Série</p>
                                 <p>{funcaoData(item.first_air_date)}</p>
                             </div>
                         </div>
@@ -32,8 +35,8 @@ export const Item2 = ({ data }: Props) => {
                     {
                         item.media_type === "person" &&
                         <div>
-                            <img src={`https://image.tmdb.org/t/p/w500/${item.profile_path}`} alt={item.original_name} />
-                            <div>
+                            <img src={`https://image.tmdb.org/t/p/w500/${item.profile_path}`} alt={item.original_name} className="hover:rounded-t-lg transition-all"/>
+                            <div className="pl-2 pb-2">
                                 <h2 className="font-bold pt-2">{item.name}</h2>
                             </div>
                         </div>
